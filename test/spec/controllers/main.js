a91d962a40b3cd6dc6dfc5817ae861b4ef67c507
Droogans <droogans@gmail.com>
2014-08-16 12:04:12 -0500

    feat(dice): Basic rolls and modifiers.
    More tests are needed.
    
    There's a lot of work yet to be done before some new
    features can be included. For starters, this controller
    does too much. It should split into several smaller units,
    namely, a constant for roll types, modifiers, and regexes.
    A new controller for handling just rolls, which includes
    meta-data about the roll (any rerolls, success details, etc.).
    Finally, a roll printer should report these in a table that
    populates as you continue rolling.
    
    Future features:
    
    Old rolls are saved to local storage.
    Stashing and popping roll sets from local storage.
    Old rolls populate into a type-ahead for auto-complete.
    A table displays roll input, meta-data, and results.

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -5,8 +5,10 @@ describe('Controller: MainCtrl', function () {
   // load the controller's module
   beforeEach(module('diceApp'));
 
-  var MainCtrl,
-    scope;
+  var MainCtrl;
+  var scope;
+  var rollTypes = ['D', 'H', 'L', 'I', 'U', 'N'];
+  var rollModifiers = ['Z', 'R', 'E'];
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
@@ -16,7 +18,126 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.awesomeThings.length).toBe(3);
+  it('should make a single choice from one die', function () {
+    expect(scope.choices([1], 1)).to.eql([1]);
   });
+
+  it('should make multiple choices from multiple dice', function () {
+    expect(scope.choices([1], 2)).to.eql([1, 1]);
+  });
+
+  describe('rollType', function () {
+
+    rollTypes.forEach(function (rollType) {
+
+      describe(rollType, function () {
+        var roll;
+
+        before(function () {
+          roll = scope.rollTypes[rollType].fn(3, 3);
+        });
+
+        it('should have a hint', function () {
+          if (['D'].indexOf(rollType) > -1) {
+            expect(scope.rollTypes[rollType].hint).to.equal('');
+          } else {
+            expect(scope.rollTypes[rollType].hint).to.not.be.empty;
+          }
+        });
+
+        it('should have the correct number of dice in the result', function () {
+          if (['H', 'L'].indexOf(rollType) > -1) {
+            expect(roll.length).to.equal(1);
+          } else if (['I'].indexOf(rollType) > -1) {
+            expect(roll.length).to.equal(2);
+          } else {
+            expect(roll.length).to.equal(3);
+          }
+        });
+
+        it('should the correct number on the face of the result', function () {
+          if (rollType === 'U') {
+            expect(roll[0]).to.be.at.least(-1);
+          } else {
+            expect(roll[0]).to.be.at.least(1);
+          }
+        });
+      });
+    });
+
+    describe('N (unique rolls)', function () {
+
+      it('should return all faces if count is greater than faces', function () {
+        expect(scope.rollTypes.N.fn(3, 4)).to.eql([1, 2, 3]);
+      });
+    });
+
+  });
+
+  describe('regular expressions', function () {
+
+    it('should properly build robust regexes', function () {
+      expect(scope.upperLowerRegex(['A', 'B', 'C'])).to.eql('[ABCabc]');
+    });
+
+  });
+
+  describe('tokenization', function () {
+
+    it('should tokenize one die', function () {
+
+    });
+
+    it('should tokenize ten dice', function () {
+
+    });
+
+    rollTypes.forEach(function (rollType) {
+
+      it('should tokenize properly for ' + rollType, function () {
+
+      });
+
+    });
+
+  });
+
+  describe('roll modifier', function () {
+
+    rollModifiers.forEach(function (rollModifier) {
+
+      describe('hints', function () {
+
+        it(rollModifier + ' should have a hint', function () {
+          expect(scope.rollModifiers[rollModifier].hint).to.not.be.empty;
+        });
+
+      });
+
+      rollTypes.forEach(function (rollType) {
+
+        describe(rollModifier + ' with ' + rollType, function () {
+          var roll;
+
+          before(function () {
+            roll = scope.rollTypes[rollType].fn(3, 3);
+          });
+
+
+        });
+
+      }
+
+    });
+
+  });
+
+  describe('roll hints', function () {
+
+  });
+
+  describe('rolling', function () {
+
+  });
+
 });
